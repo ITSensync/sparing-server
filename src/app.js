bcce7c4f5d81276f76a30f4578d01cf631b1cd88
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-07-09 16:01:57 +0700

    chore: disable sync db user telegram

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const middlewares = require('./middlewares');
 const api = require('./api');
 // const { WaterQuality } = require('./model/WaterQuality');
-const { UserTelegram } = require('./model/UserTelegram');
+// const { UserTelegram } = require('./model/UserTelegram');
 // const cronJob = require('./scheduler/cron-job');
 const { Device } = require('./model/Device');
 const { WaterLevel } = require('./model/WaterLevel');
@@ -27,7 +27,7 @@ app.use(express.json());
     await sequelize.authenticate();
     // WaterQuality.sync({ alter: true });
     WaterLevel.sync({ alter: true });
-    UserTelegram.sync();
+    // UserTelegram.sync();
     Device.sync({ alter: true });
     console.log('Connection to the database has been established successfully.');
   } catch (error) {
